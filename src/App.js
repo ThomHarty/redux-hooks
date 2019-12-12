22c6ba72ac133ef0e5b0b61a2560c2724e48ea8e
Tommy Harty <tommyharty@tommys-mbp.home>
2019-12-12 21:47:16 +0000

    refactor posts component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { View } from 'react-native'
 
+import Posts from './components/Posts'
+
 import reducers from './reducers'
 import { Provider, useSelector, useDispatch } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
@@ -18,21 +20,6 @@ const App = () => {
     <Provider store={store}>
       <Posts />
     </Provider>
-  )
-}
-
-const Posts = () => {
-  const posts = useSelector(state => state.posts)
-  const users = useSelector(state => state.users)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-      dispatch({ type: 'posts/get_posts_request' })
-    }, [])
-
-  console.log('component level posts: ', posts)
-  return (
-    <View></View>
   )
 }
 
