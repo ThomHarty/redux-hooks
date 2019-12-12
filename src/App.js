e587cb702e6fdce507285c1aea14cfe3a19bd48f
Tommy Harty <tommyharty@tommys-mbp.home>
2019-12-12 18:39:59 +0000

    add redux, actions, reducers, store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import reducers from './reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import {
   usePostsApiCall,
   useUsersApiCall,
-} from './api/library'
+} from './api'
+
+const store = createStore(reducers)
 
 const App = () => {
   const [posts] = usePostsApiCall()
   const [users] = useUsersApiCall()
 
-  console.log('posts: ', posts)
-  console.log('users: ', users)
+  console.log('store: ', store)
 
   return (
-    <View></View>
+    <Provider store={store}>
+      <View></View>
+    </Provider>
   )
 }
 
--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-import { url } from '../config'
+import { url } from './config'
 
 export default function usePostsApiCall() {
   const [posts, setPosts] = useState([])
