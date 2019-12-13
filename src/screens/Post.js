22b853eb5131181137f3700acee285ce95fa5519
Tommy Harty <tommyharty@tommys-mbp.home>
2019-12-13 01:05:58 +0000

    single post navigation

--- src/screens/Post.js
+++ src/screens/Post.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, ScrollView } from 'react-native'
 
-const Post = () => {
+const Post = ({ navigation }) => {
+  const post = navigation.getParam('post')
+  const user = navigation.getParam('user')
+
   return (
     <SafeAreaView>
-      <Text>Post</Text>
+      <ScrollView>
+        <Text>{post.title}</Text>
+        <Text>{post.body}</Text>
+        <Text>{user.name}</Text>
+      </ScrollView>
     </SafeAreaView>
   )
 }
